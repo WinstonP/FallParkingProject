351e968a8abfffdf0ca0eca516d24d319fb7880b
winston parris <winstonparris@macbook-pro-2.xavier.xula.local>
2016-10-04 17:35:24 -0500

    now support mustache templates

--- testspark/src/main/java/ParkApp.java
+++ testspark/src/main/java/ParkApp.java
@@ -2,9 +2,11 @@
  * Created by winstonparris on 10/3/16.
  */
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import spark.*;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -15,8 +17,18 @@ public class ParkApp {
         get("/hello", new Route() {
             public Object handle(Request req, Response res) throws Exception {
                 return "hey winston";
+
             }
         });
+        // hello.mustache file is in resources/templates directory
+        get("/template", new TemplateViewRoute() {
+            public ModelAndView handle(Request rq, Response rs) throws Exception {
+
+                Map map = new HashMap();
+                map.put("name", "foobarfoo");
+                return new ModelAndView(map, "hello.mustache");
+            }
+        }, new MustacheTemplateEngine());
     }
 
 }
